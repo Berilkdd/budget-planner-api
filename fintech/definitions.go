a244ad135c71b04b21efb755a792dbe14531abb0
Berilkdd <brlkrdnz@hotmail.com>
2026-08-16 02:46:00 +0100

    refactor: rename errors definitions file

--- fintech/definitions.go
+++ fintech/definitions.go
@@ -1,4 +1,5 @@
 package fintech
+
 import "errors"
 
 var ErrZeroIncome = errors.New("income must be greater than zero")
@@ -12,31 +13,35 @@ var ErrSubscriptionFeeDeficit = errors.New("monthly savings allocation is too lo
 type WarningCode string
 
 const (
-	WarningLowWantsBudget                  WarningCode = "LOW_WANTS_BUDGET"
-	WarningNeedsExceedIncome               WarningCode = "NEEDS_EXCEED_INCOME"
+	WarningLowWantsBudget    WarningCode = "LOW_WANTS_BUDGET"
+	WarningNeedsExceedIncome WarningCode = "NEEDS_EXCEED_INCOME"
 
-	WarningNeedsEqualIncome                WarningCode = "NEEDS_EQUAL_INCOME"
-	WarningBelowEmergencyFund              WarningCode = "BELOW_EMERGENCY_FUND"
-	WarningEmergencyFundCovered            WarningCode = "EMERGENCY_FUND_COVERED"
-	WarningSavingsMayBeUsed                WarningCode = "SAVINGS_MAY_BE_USED"
-	WarningUnsettledDebt                   WarningCode = "UNSETTLED_DEBT"
-	WarningNoUnsettledDebt                 WarningCode = "NO_UNSETTLED_DEBT"	
+	WarningNeedsEqualIncome     WarningCode = "NEEDS_EQUAL_INCOME"
+	WarningBelowEmergencyFund   WarningCode = "BELOW_EMERGENCY_FUND"
+	WarningEmergencyFundCovered WarningCode = "EMERGENCY_FUND_COVERED"
+	WarningSavingsAtRisk        WarningCode = "SAVINGS_AT_RISK"
+	WarningUnsettledDebt        WarningCode = "UNSETTLED_DEBT"
+	WarningNoUnsettledDebt      WarningCode = "NO_UNSETTLED_DEBT"
 
-	WarningNeedsBelow50 WarningCode = "NEEDS_BELOW_50"
-	WarningNeedsEqual50 WarningCode = "NEEDS_EQUAL_50"
-	WarningNeedsBelow60 WarningCode = "NEEDS_BELOW_60"
-	WarningNeedsEqual60 WarningCode = "NEEDS_EQUAL_60"
-	WarningNeedsAbove60 WarningCode = "NEEDS_ABOVE_60"
+	WarningNeedsBelow50        WarningCode = "NEEDS_BELOW_50"
+	WarningNeedsEqual50        WarningCode = "NEEDS_EQUAL_50"
+	WarningNeedsBetween50And60 WarningCode = "NEEDS_BELOW_60"
+	WarningNeedsEqual60        WarningCode = "NEEDS_EQUAL_60"
+	WarningNeedsAbove60        WarningCode = "NEEDS_ABOVE_60"
 )
 
 // Action codes
 type ActionCode string
+type Action struct {
+	Code        ActionCode
+	Description string
+}
 
 const (
-	ActionSupportAdvised             ActionCode = "SUPPORT_ADVISED"
-	ActionDebtAdviceAdvised          ActionCode = "DEBT_ADVICE_ADVISED"
-	ActionFullDebtPaymentAdvised     ActionCode = "FULL_DEBT_PAYMENT_ADVISED"
-	ActionPartialDebtPaymentAdvised  ActionCode = "PARTIAL_DEBT_PAYMENT_ADVISED"
+	ActionSupportAdvised            ActionCode = "SUPPORT_ADVISED"
+	ActionDebtAdviceAdvised         ActionCode = "DEBT_ADVICE_ADVISED"
+	ActionFullDebtPaymentAdvised    ActionCode = "FULL_DEBT_PAYMENT_ADVISED"
+	ActionPartialDebtPaymentAdvised ActionCode = "PARTIAL_DEBT_PAYMENT_ADVISED"
 )
 
 var ActionDefinitions = map[ActionCode]Action{
@@ -66,52 +71,51 @@ type Warning struct {
 	Description string
 }
 
-
 // Warning definitions
 var WarningDefinitions = map[WarningCode]Warning{
 	WarningLowWantsBudget: {
-		Code: WarningLowWantsBudget,
+		Code:        WarningLowWantsBudget,
 		Description: "Discretionary money is below £100 per month for a debt repayment period of 6 months or longer.",
 	},
-	
+
 	WarningNeedsExceedIncome: {
-		Code: WarningNeedsExceedIncome,
+		Code:        WarningNeedsExceedIncome,
 		Description: "Essential needs exceed monthly income.",
 	},
 
 	WarningNeedsEqualIncome: {
-		Code: WarningNeedsEqualIncome,
+		Code:        WarningNeedsEqualIncome,
 		Description: "Essential needs meet monthly income.",
 	},
 
 	WarningBelowEmergencyFund: {
-		Code: WarningBelowEmergencyFund,
+		Code:        WarningBelowEmergencyFund,
 		Description: "Current savings are below the required emergency fund.",
 	},
 
 	WarningEmergencyFundCovered: {
-		Code: WarningEmergencyFundCovered,
+		Code:        WarningEmergencyFundCovered,
 		Description: "Current savings cover the required emergency fund.",
 	},
 
-	WarningSavingsMayBeUsed: {
-		Code: WarningSavingsMayBeUsed,
+	WarningSavingsAtRisk: {
+		Code:        WarningSavingsAtRisk,
 		Description: "Savings may be needed to cover the gap between essential expenses and income over time.",
 	},
 
 	WarningUnsettledDebt: {
-		Code: WarningUnsettledDebt,
+		Code:        WarningUnsettledDebt,
 		Description: "Outstanding debt remains.",
 	},
 
 	WarningNoUnsettledDebt: {
-		Code: WarningNoUnsettledDebt,
+		Code:        WarningNoUnsettledDebt,
 		Description: "No outstanding debt remains.",
-	},	
+	},
 
 	WarningNeedsBelow50: {
-	Code:        WarningNeedsBelow50,
-	Description: "Needs below 50% of income.Healthy.",
+		Code:        WarningNeedsBelow50,
+		Description: "Needs below 50% of income. Healthy.",
 	},
 
 	WarningNeedsEqual50: {
@@ -119,8 +123,8 @@ var WarningDefinitions = map[WarningCode]Warning{
 		Description: "Needs equal 50% of income.Healthy.",
 	},
 
-	WarningNeedsBelow60: {
-		Code:        WarningNeedsBelow60,
+	WarningNeedsBetween50And60: {
+		Code:        WarningNeedsBetween50And60,
 		Description: "Needs above 50% but below 60% of income. Reasonable.",
 	},
 
